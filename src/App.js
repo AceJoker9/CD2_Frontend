2261bf1eec930d64383fca43c9ebcc1350333770
unknown <neilmagic25@gmail.com>
2023-07-08 20:26:14 -0400

    Finished the Music Frontend Assignment

--- src/App.js
+++ src/App.js
@@ -1,44 +1,67 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import NavigationBar from "./NavigationBar";
 import SearchBar from "./SearchBar";
 import MusicTable from "./MusicTable";
 
-
-
-
-let songs = [
-  {"id": 1, "title": "Billie Jean", "artist": "Michael Jackson", "album": "Thriller", "release_date": "1982", "genre": "R&B"},
-  {"id": 2, "title": "Purple Rain", "artist": "Prince", "album": "Purple Rain", "release_date": "1984", "genre": "R&B"},
-  {"id": 3, "title": "Super Freak", "artist": "Rick James", "album": "Street Songs", "release_date": "1981", "genre": "R&B"},
-  {"id": 4, "title": "I Wanna Dance with Somebody", "artist": "Whitney Houston", "album": "I'm your baby tonight", "release_date": "1981", "genre": "R&B"},
-  {"id": 5, "title": "Rock with You", "artist": "Michael Jackson", "album": "Off The Wall", "release_date": "1979", "genre": "R&B"},
-  {"id": 6, "title": "Sweet Child o' Mine", "artist": "Guns N' Roses", "album": "Appetite for Destruction", "release_date": "1987", "genre": "R&B"},
-]
-
-function App () {
+function App() {
   const [songs, setSongs] = useState([]);
-
+  const [filteredSongs, setFilteredSongs] = useState([]);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     getAllSongs();
-    console.log('Hello World');
-},[]);
+  }, []);
 
   async function getAllSongs() {
-    const response = await axios.get('http://127.0.0.1:8000/api/tracks/');
-    console.log (response.data);
-    setSongs(response.data)
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/tracks/");
+      setSongs(response.data);
+      setFilteredSongs(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   }
+
+  function handleFilterChange(event) {
+    setFilter(event.target.value);
+    filterSongs(event.target.value);
+  }
+
+  function filterSongs(filter) {
+    if (filter === "") {
+      setFilteredSongs(songs);
+    } else {
+      const filtered = songs.filter(
+        (song) =>
+          song.title.toLowerCase().includes(filter.toLowerCase()) ||
+          song.album.toLowerCase().includes(filter.toLowerCase()) ||
+          song.artist.toLowerCase().includes(filter.toLowerCase()) ||
+          song.genre.toLowerCase().includes(filter.toLowerCase()) ||
+          song.release_date.toLowerCase().includes(filter.toLowerCase())
+      );
+      setFilteredSongs(filtered);
+    }
+  }
+
+  async function handleDelete(id) {
+    try {
+      await axios.delete(`http://127.0.0.1:8000/api/tracks/delete/${id}/`);
+      setSongs(songs.filter((song) => song.id !== id));
+      setFilteredSongs(filteredSongs.filter((song) => song.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
       <NavigationBar />
-      <SearchBar />
-      <MusicTable />
-      
-      <button onClick [() => getAllSongs()] > Get All Songs </button>
+      <SearchBar filter={filter} handleFilterChange={handleFilterChange} />
+      <MusicTable songs={filteredSongs} onDelete={handleDelete} />
+      <button onClick={addSong}>Add Song</button>
     </div>
   );
-};
+}
 
 export default App;
--- src/MusicTable.js
+++ src/MusicTable.js
@@ -1,11 +1,50 @@
 import React from "react";
+import axios from "axios";
 
-const MusicTable = () => {
+const MusicTable = ({ songs, onDelete }) => {
+
+  async function handleDelete(id) {
+    try {
+      await axios.delete(`http://127.0.0.1:8000/api/tracks/delete/${id}/`);
+      onDelete(id);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
-      {/* Music table content */}
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Release Date</th>
+            <th>Genre</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {songs.map((song) => (
+            <tr key={song.id}>
+              <td>{song.title}</td>
+              <td>{song.artist}</td>
+              <td>{song.album}</td>
+              <td>{song.release_date}</td>
+              <td>{song.genre}</td>
+              <td>
+                <button onClick={() => handleDelete(song.id)}>Delete</button>
+                {/* Add button or link to trigger modal for editing */}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
 export default MusicTable;
+
+
